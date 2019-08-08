37a78f770faf090fd7530881df397045aacb2ce8
lisatostrams <lmc.tostrams@student.ru.nl>
2019-08-08 17:08:44 +0200

    Update orderbook.py

--- get_binance_data/orderbook.py
+++ get_binance_data/orderbook.py
@@ -8,7 +8,7 @@ Created on Mon Aug  5 23:47:42 2019
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import copy
 data =  open("LINK_USDT_notick.json").read().splitlines()
 
 deltas = []
@@ -27,6 +27,21 @@ df.index = df.timestamp
 df = df.rename(columns = {"a": "ask", "b":"bid"})
 
 trades = []
+
+#%%
+
+def extract_book_features(book):
+    if len(book["ask"]) == 0 and len(book["bid"]) == 0:
+        return 0,0
+    asks = sorted(book["ask"].items())
+    bids = sorted(book["bid"].items(), reverse=True)
+    min_ask = asks[0][0]
+    max_bid = bids[0][0]
+    distance = min_ask - max_bid
+    midprice = (min_ask + max_bid) / 2
+    
+    
+    return distance, min_ask, max_bid, midprice
 #%%
 orderbook = {
 		"ask": {},
@@ -37,6 +52,7 @@ orderbook = {
 seconds = pd.date_range(start=df['timestamp'].min(), end=df['timestamp'].max(), freq='s')
 #books = pd.DataFrame(columns=['ask','bid'],index=seconds)
 books = []
+book_features = []
 
 for s in seconds:
 	mask = ((df["timestamp"] > s) & (df["timestamp"] < s + pd.Timedelta(1, unit='s')))
@@ -52,11 +68,30 @@ for s in seconds:
 				elif price in orderbook[side]:
 					orderbook[side].pop(price)
 		print(len(orderbook["ask"]), len(orderbook["bid"]))
-	book = orderbook
+	book = copy.deepcopy(orderbook)
 	book["ts"] = s
+	book_features.append(extract_book_features(book))
 	books.append(book)
 	
 books = pd.DataFrame(books)
 books.set_index("ts")
 books.to_csv("orderbooks_link_usdt.csv")
+
+#%%
+def plot_2(y):
+    x = np.arange(0,len(y),1)
+    plt.plot(x,[z[0] for z in y])
+    plt.plot(x,[z[1] for z in y])
+    plt.show()
+    
+def plot_1(y):
+    x = np.arange(0,len(y),1)
+    plt.plot(x,y)
+    plt.show()
+
+#%%
+plot_1([x[0] for x in book_features[2:1000]])
+
+plot_1([x[3] for x in book_features[2:1000]])
+plot_2([(x[1], x[2]) for x in book_features[2:1000]])
 #%%
